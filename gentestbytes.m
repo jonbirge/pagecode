a1ade9de9e5050ba54d372fe54fddd109d99625f
Jonathan Birge <birge@mit.edu>
2022-10-28 23:42:10 -0400

    rename testbytes.m

---
+++ gentestbytes.m
@@ -0,0 +1,4 @@
+function xs = testbytes(n)
+
+t = linspace(0, 8*pi, n);
+xs = uint8(255*(-cos(t)/2 + 0.5));
--- testbytes.m
+++
@@ -1,4 +0,0 @@
-function xs = testbytes(n)
-
-t = linspace(0, 8*pi, n);
-xs = uint8(255*(-cos(t)/2 + 0.5));
