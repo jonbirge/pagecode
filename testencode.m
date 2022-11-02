03a8312d96511fa7bde8128b73594520aa41edf7
Jonathan Birge <birge@mit.edu>
2022-11-01 23:19:08 -0400

    working page encode test

---
+++ testencode.m
@@ -0,0 +1,33 @@
+% Generate data
+clearvars
+nbytes = 1024;
+data = uint8(randi(255, 1, nbytes));
+
+% Generate barcode
+pixperbyte = 4*8/2;
+overheadpix = 5*pixperbyte;
+linepix = 10;  % pixels per barcode frame
+dpi = 300;
+win = 6;  % inches
+hin = 8;  % inches
+framebytes = floor((300*6 - overheadpix)/pixperbyte);
+nframes = ceil(nbytes/framebytes);
+
+% header frame
+xhead = ones(1, framebytes);
+headfrm = encodeframe(xhead);
+framepix = length(headfrm);
+
+% all frames
+codim = uint8(zeros(nframes + 1, framepix));
+codim(1,:) = headfrm;
+for k = 1:nframes
+  idx1 = (k-1)*framebytes + 1;
+  idx2 = min(idx1 + framebytes - 1, nbytes);
+  frmdat = data(idx2:idx2);
+  frm = encodeframe(frmdat);
+  codim(k+1,1:length(frm)) = frm;
+end
+
+% Save to PNG file
+imagesc(codim)
