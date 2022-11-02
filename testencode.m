c850225435ff4fd61a9a5f98405f03d1272cd555
Jonathan Birge <birge@mit.edu>
2022-11-01 23:19:29 -0400

    fix add

--- testencode.m
+++ testencode.m
@@ -1,15 +1,19 @@
-% Generate data
+%% Generate data
+
 clearvars
-nbytes = 1024;
+nbytes = 20*1024;
 data = uint8(randi(255, 1, nbytes));
 
-% Generate barcode
+
+%% Generate barcode
+
 pixperbyte = 4*8/2;
 overheadpix = 5*pixperbyte;
 linepix = 10;  % pixels per barcode frame
-dpi = 300;
+dpi = 200;
 win = 6;  % inches
 hin = 8;  % inches
+pageframes = floor(300*8/linepix);
 framebytes = floor((300*6 - overheadpix)/pixperbyte);
 nframes = ceil(nbytes/framebytes);
 
@@ -24,10 +28,13 @@ codim(1,:) = headfrm;
 for k = 1:nframes
   idx1 = (k-1)*framebytes + 1;
   idx2 = min(idx1 + framebytes - 1, nbytes);
-  frmdat = data(idx2:idx2);
+  frmdat = data(idx1:idx2);
   frm = encodeframe(frmdat);
   codim(k+1,1:length(frm)) = frm;
 end
 
-% Save to PNG file
-imagesc(codim)
+
+%% Save to file
+
+imagesc(1 - codim)
+colormap('gray')
