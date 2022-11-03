c17560840ad58bd721f0b2c85649a9020df92630
Jonathan Birge <birge@mit.edu>
2022-11-03 00:27:08 -0400

    minor fixes

--- testencode.m
+++ testencode.m
@@ -1,21 +1,24 @@
 %% Generate data
 
 clearvars
-nbytes = 20*1024;
+nbytes = 8*1024;
 data = uint8(randi(255, 1, nbytes));
 
 
-%% Generate barcode
+%% Parameters
 
 pixperbyte = 4*8/2;
 overheadpix = 5*pixperbyte;
-linepix = 10;  % pixels per barcode frame
-dpi = 200;
+linepix = 5;  % pixels per barcode frame
+dpi = 150;
 win = 6;  % inches
 hin = 8;  % inches
-pageframes = floor(300*8/linepix);
-framebytes = floor((300*6 - overheadpix)/pixperbyte);
+pageframes = floor(dpi*hin/linepix);
+framebytes = floor((dpi*win - overheadpix)/pixperbyte);
 nframes = ceil(nbytes/framebytes);
+
+
+%% Generate code
 
 % header frame
 xhead = ones(1, framebytes);
@@ -23,18 +26,19 @@ headfrm = encodeframe(xhead);
 framepix = length(headfrm);
 
 % all frames
-codim = uint8(zeros(nframes + 1, framepix));
-codim(1,:) = headfrm;
+codim = false(linepix*(nframes + 1), framepix);
+codim(1:linepix,:) = repmat(headfrm, linepix, 1);
 for k = 1:nframes
   idx1 = (k-1)*framebytes + 1;
   idx2 = min(idx1 + framebytes - 1, nbytes);
   frmdat = data(idx1:idx2);
-  frm = encodeframe(frmdat);
-  codim(k+1,1:length(frm)) = frm;
+  frm = logical(encodeframe(frmdat));
+  linidx1 = k*linepix;
+  linidx2 = linidx1 + linepix - 1;
+  codim(linidx1:linidx2,1:length(frm)) = repmat(frm, linepix, 1);
 end
 
 
-%% Save to file
+%% Save to file(s)
 
-imagesc(1 - codim)
-colormap('gray')
+imwrite(codim, 'testbar.png');
