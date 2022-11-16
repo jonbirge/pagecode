ee4af4e5a0bf2d1cb1fe111f2d4aa9f2045f5561
Jonathan Birge <birge@mit.edu>
2022-11-16 00:24:21 -0500

    refactored code

--- testdecodepage.m
+++ testdecodepage.m
@@ -1,59 +1,29 @@
-%% Generate data
+%% Generate code
 
+% test parameters
 clearvars
 nbytes = 16*1024;
 data = uint8(gensawbytes(nbytes));
 
 % parameters
-dpi = 98;
-win = 6.5;  % inches
-hin = 9;  % inches
-linepix = 2;  % pixels per barcode frame
-printsnr = 10;
+codepar.dpi = 98;
+codepar.win = 6.5;  % inches
+codepar.hin = 9;  % inches
+codepar.linepix = 2;  % pixels per barcode frame
 
-nchip = 4;
-bitsperchip = 2;
-pixperbyte = nchip*8/bitsperchip;
-overheadpix = 5*nchip;
-pageframes = floor(dpi*hin/linepix);
-framebytes = floor((dpi*win - overheadpix)/pixperbyte);
-nframes = ceil(nbytes/framebytes);
-
-
-%% Generate codes
-
-% TODO: put file name in header
-
-% header frame
-xhead = 42*ones(1, framebytes);
-headfrm = encodeframe(xhead, 0);
-framepix = length(headfrm);
-
-% TODO: have encodeframe take an optional parameter that is the upper limit
-% on line length, and return number of pixels used. build code iteratively.
-
-% data frames
-codim = false(linepix*(nframes + 1), framepix);
-codim(1:linepix,:) = repmat(headfrm, linepix, 1);
-for k = 1:nframes
-  idx1 = (k-1)*framebytes + 1;
-  idx2 = min(idx1 + framebytes - 1, nbytes);
-  frmdat = data(idx1:idx2);
-  frm = encodeframe(frmdat, k);
-  linidx1 = k*linepix + 1;
-  linidx2 = linidx1 + linepix - 1;
-  codim(linidx1:linidx2,1:length(frm)) = repmat(frm, linepix, 1);
-  %figure(3)
-  %imagesc(codim(1:32,1:end))
-end
+% generate page
+pageframes = floor(codepar.dpi*codepar.hin/codepar.linepix);
+codim = encodepage(data, codepar);
 
 
 %% Simulate print
 
+% print parameters
+printsnr = 4;
 pwin = 8.5;
 phin = 11;
-wbuf = (pwin - win)/win;
-hbuf = (phin - hin)/hin;
+wbuf = (pwin - codepar.win)/codepar.win;
+hbuf = (phin - codepar.hin)/codepar.hin;
 im = simpage(codim, printsnr, hbuf, wbuf);
 
 % figure(1)
@@ -67,97 +37,25 @@ im = simpage(codim, printsnr, hbuf, wbuf);
 
 imscan = logical(round(im));
 
-% figure(1)
+% figure(2)
 % imagesc(imscan)
 
 
 %% Read and normalize simulated scan
 
-% TODO: should check if image is binary, and if not, high-pass and quantize
-% the data to logical array.
-
-% get rid of trivial white-space
-[nh, nw] = size(imscan);
-rowsum = sum(imscan, 2);
-improc = imscan(rowsum > 32, :);
-
-% find fiducial edges. (this is not a robust algorithm.)
-[nhp, ~] = size(improc);
-k0 = zeros(1, nhp);
-for k = 1:nhp
-  imline = improc(k,:);
-  k0(k) = find(imline,1);
-end
-
-% remove outliers beyond 10 pixels
-kmed = median(k0, 'omitnan');
-k0(abs(k0 - kmed) > 10) = NaN;
-
-% figure(1)
-% plot(k0)
-
-% fit line to edges, assuming square pixels
-lm = fitlm(1:nhp, k0);
-figure(1)
-plot(lm)
-slope = lm.Coefficients.Estimate(2);
-
-% square edges
-whitesp = ceil(min(predict(lm, (1:nhp).')));  % how lazy am i?
-imcrop = improc(1:end, whitesp:end);
-
-% plot cropped result
-figure(2)
-imagesc(imcrop)
-
-
-%% Decode lines
-
-% TODO: we either need to collapse
-% everything to a logical earlier, or figure out a way to handle
-% grey-scale. one option would be to pull out the preprocess code from
-% decode frame and put it here, and have that handle decide what's a line
-% and what's not.
-
-% TODO: read all similar lines into buffer and vote.
-
-% read lines, assuming 'imcrop' is logical
-[nhc, ~] = size(imcrop);
-rawdata = {};
-ids = [];
-dataline = 1;
-for k = 1:nhc
-  if mod(k, 100) == 0
-    fprintf('k: %d\n', k)
-  end
-  if ~isnan(k0(k))
-    imline = imcrop(k,:);
-    [xs, nsamp] = decodeframe(imline);
-    rawdata{dataline} = xs(2:end); %#ok<SAGROW>
-    ids(dataline) = xs(1); %#ok<SAGROW> 
-    dataline = dataline + 1;
-  end
-end
-
-
-%% Combine lines
-
-% for now, just take the first id, and make the assumption that all ids are
-% valid and that the only job is to take the first and move on. in the
-% future, a more robust id system could be developed, and all lines voted
-% with the same id would vote per chip.
-
-nids = length(ids);  % number of lines decoded
-xs = [];
-lastid = 0;
-kline = 1;
-for k = 1:nids
-  id = ids(k);
-  if id ~= lastid
-    xs = [xs, rawdata{k}]; %#ok<AGROW>
-    lastid = id;
-  end
-end
+dataout = decodepage(imscan);
 
 figure(3)
-plot(xs)
+plot(dataout)
+
+
+%% Evaluation
+
+nout = length(dataout);
+if nout ~= nbytes
+  fprintf('data lost!\n');
+else
+  errs = abs(dataout - data);
+  nerrs = sum(errs > 0);
+  fprintf('byte errors = %d\n', nerrs);
+end
